416add6bddb77c3a89a6aca6447a36fa6edcf191
mdmoreau <mdmoreau@gmail.com>
2015-07-20 13:08:21 -0400

    removed front-matter

--- gulp/tasks/nunjucks.js
+++ gulp/tasks/nunjucks.js
@@ -1,18 +1,15 @@
 var browserSync = require('browser-sync').get('server');
 var gulp = require('gulp');
-var frontMatter = require('gulp-front-matter');
 var minifyHtml = require('gulp-minify-html');
 var nunjucksHtml = require('gulp-nunjucks-html');
 var plumber = require('gulp-plumber');
 var config = require('../config');
 var error = require('../util/error');
 
-// render and optimize nunjucks templates to html using inline yaml
+// render and optimize nunjucks templates to html
 gulp.task('nunjucks', function() {
   return gulp.src(config.nunjucks.src)
     .pipe(plumber())
-    .pipe(frontMatter({property: 'data'}))
-    .on('error', error)
     .pipe(nunjucksHtml(config.nunjucks.opts))
     .on('error', error)
     .pipe(minifyHtml())

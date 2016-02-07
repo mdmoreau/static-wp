b7a6ec7d9c2f3a4984e042a4ef0a34ed473ade5b
mdmoreau <mdmoreau@gmail.com>
2016-02-06 21:30:27 -0500

    jquery dependencies removed

--- src/js/script.js
+++ src/js/script.js
@@ -12,17 +12,6 @@ var requestAnimationFrame = require('raf');
 var svg4everybody = require('svg4everybody');
 svg4everybody();
 
-// global jquery
-global.jQuery = require('jquery');
-
 // flickity with imagesloaded
 var Flickity = require('flickity');
 require('flickity-imagesloaded');
-
-// bridget turns flickity into a jquery plugin
-require('jquery-bridget');
-jQuery.bridget('flickity', Flickity);
-
-// placeholder attribute polyfill
-require('jquery-placeholder');
-jQuery('input, textarea').placeholder();

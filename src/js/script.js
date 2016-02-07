de4c1eb7870dadc780de46c76d25c4d25ce5f064
mdmoreau <mdmoreau@gmail.com>
2016-02-07 15:56:23 -0500

    remove flickity and raf, modernizr optional

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,10 @@
 // customized modernizr build
-require('./util/_modernizr');
+// require('./util/_modernizr');
 
 // fastclick eliminates click delay in certain browsers
 var fastclick = require('fastclick');
 fastclick(document.body);
 
-// raf polyfill
-var requestAnimationFrame = require('raf');
-
 // svg polyfill for better xlink support
 var svg4everybody = require('svg4everybody');
 svg4everybody();
-
-// flickity with imagesloaded
-var Flickity = require('flickity');
-require('flickity-imagesloaded');

6be41d3fb4b0c211c22464cfc2dae16bbb148403
mdmoreau <mdmoreau@gmail.com>
2016-02-08 22:52:00 -0500

    sass sourcemaps

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -4,17 +4,20 @@ var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var sassGlob = require('gulp-sass-glob');
+var sourcemaps = require('gulp-sourcemaps');
 var config = require('../config');
 var error = require('../util/error');
 
-// process, prefix, and optimize sass files
+// process, prefix, and optimize sass files with sourcemaps
 gulp.task('sass', function() {
   return gulp.src(config.sass.src)
     .pipe(sassGlob())
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .on('error', error)
     .pipe(autoprefixer())
     .pipe(minifyCss({advanced: false}))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.sass.dist))
     .pipe(browserSync.stream());
 });

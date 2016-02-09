dfe474b95595465c026de427861a3c256800fe34
mdmoreau <mdmoreau@gmail.com>
2016-02-08 19:57:23 -0500

    browserify require globbing

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,6 @@ fastclick(document.body);
 // svg polyfill for better xlink support
 var svg4everybody = require('svg4everybody');
 svg4everybody();
+
+// automatically require all modules
+require('./modules/*.js', {mode: 'expand'});

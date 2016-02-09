868f15f5758364149de39f7d61e087749bc6a2bf
mdmoreau <mdmoreau@gmail.com>
2016-02-08 20:47:07 -0500

    sass import globbing

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -3,12 +3,14 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
 var config = require('../config');
 var error = require('../util/error');
 
 // process, prefix, and optimize sass files
 gulp.task('sass', function() {
   return gulp.src(config.sass.src)
+    .pipe(sassGlob())
     .pipe(sass())
     .on('error', error)
     .pipe(autoprefixer())

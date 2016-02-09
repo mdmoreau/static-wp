02447b8e6cd8611f0d39e538729b73dd11aa9f8b
mdmoreau <mdmoreau@gmail.com>
2016-02-09 14:35:21 -0500

    sass build simplified

--- gulpfile.js/tasks/sass-init.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-
-// initial sass task to create dependent source files in the correct order
-gulp.task('sass-init', function(cb) {
-  runSequence('svgstore-sass', 'sass', cb);
-});

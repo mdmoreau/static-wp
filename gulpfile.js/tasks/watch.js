fb296f5c9d8f71ebffb92bfef1ee43407200ca94
mdmoreau <mdmoreau@gmail.com>
2016-02-09 21:48:37 -0500

    gulp syntax fix

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -6,7 +6,7 @@ gulp.task('watch', function() {
   gulp.watch(config.sass.src, gulp.registry().get('sass'));
   gulp.watch(config.fonts.src, gulp.registry().get('fonts'));
   gulp.watch(config.images.src, gulp.registry().get('images'));
-  gulp.watch(config.svgstore.src, gulp.registry().get('svgstore', 'svgstore-sass'));
+  gulp.watch(config.svgstore.src, gulp.parallel('svgstore', 'svgstore-sass'));
   gulp.watch(config.svgstore.template, gulp.registry().get('svgstore-sass'));
   gulp.watch(config.nunjucks.watch, gulp.registry().get('nunjucks'));
 });

33c9bf4eff09cd62fbb29a90a769419fd4d5a758
mdmoreau <mdmoreau@gmail.com>
2016-05-12 20:55:00 -0400

    generate non minified js

--- gulpfile.js/util/bundle.js
+++ gulpfile.js/util/bundle.js
@@ -1,5 +1,7 @@
 var browserSync = require('browser-sync').get('server');
 var gulp = require('gulp');
+var clone = require('gulp-clone');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
@@ -9,12 +11,16 @@ var error = require('../util/error');
 
 // function to bundle browserify files
 module.exports = function(b) {
+  var cloneSink = clone.sink();
   return b.bundle()
     .on('error', error)
     .pipe(source(config.browserify.bundle))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
+    .pipe(cloneSink)
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+    .pipe(cloneSink.tap())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.browserify.dist))
     .pipe(browserSync.stream({once: true}));

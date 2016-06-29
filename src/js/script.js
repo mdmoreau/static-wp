66e2c46908d6689418c7bdda3b32ee1f8b62c3d9
mdmoreau <mdmoreau@gmail.com>
2016-06-28 21:29:12 -0400

    drop fastclick

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,3 @@
-// fastclick eliminates click delay in certain browsers
-var fastclick = require('fastclick');
-fastclick(document.body);
-
 // svg polyfill for better xlink support
 var svg4everybody = require('svg4everybody');
 svg4everybody();

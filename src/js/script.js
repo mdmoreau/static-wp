85fe984c7aa1829686a8d032148405aaee37d16a
mdmoreau <mdmoreau@gmail.com>
2016-09-03 13:57:32 -0400

    add script to handle accessible outline removal

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 // svg polyfill for better use element support
 require('svgxuse');
 
+// remove outlines while maintaining accessibility
+require('./util/_outline.js');
+
 // automatically require all modules
 require('./modules/**/*.js', {mode: 'expand'});

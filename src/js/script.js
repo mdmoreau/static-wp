ac35dd8634d536fc8a0f549c2fcf7e56e4b6ccdf
mdmoreau <mdmoreau@gmail.com>
2016-10-10 16:59:09 -0400

    adjust focus styles

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@ require('svgxuse');
 
 // track input methods for styling
 require('what-input');
-document.body.setAttribute('data-whatinput-formswitching', '');
 
 // automatically require all modules
 require('./modules/**/*.js', {mode: 'expand'});

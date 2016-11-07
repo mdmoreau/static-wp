257aa41a7be4e1598923e20d57b4530c5c4eda2a
mdmoreau <mdmoreau@gmail.com>
2016-11-07 09:52:43 -0500

    remove asset minification

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -1,9 +1,6 @@
 var browserSync = require('browser-sync').get('server');
 var gulp = require('gulp');
-var cleanCss = require('gulp-clean-css');
-var clone = require('gulp-clone');
 var postcss = require('gulp-postcss');
-var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
@@ -12,17 +9,12 @@ var error = require('../util/error');
 
 // process, prefix, and optimize sass files with sourcemaps
 gulp.task('sass', function() {
-  var cloneSink = clone.sink();
   return gulp.src(config.sass.src)
     .pipe(sassGlob())
     .pipe(sourcemaps.init())
     .pipe(sass())
     .on('error', error)
     .pipe(postcss(config.sass.postcss))
-    .pipe(cloneSink)
-      .pipe(rename({suffix: '.min'}))
-      .pipe(cleanCss())
-    .pipe(cloneSink.tap())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.sass.dist))
     .pipe(browserSync.stream({match: '**/*.css'}));
--- gulpfile.js/util/bundle.js
+++ gulpfile.js/util/bundle.js
@@ -1,9 +1,6 @@
 var browserSync = require('browser-sync').get('server');
 var gulp = require('gulp');
-var clone = require('gulp-clone');
-var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
 var config = require('../config');
@@ -11,16 +8,11 @@ var error = require('../util/error');
 
 // function to bundle browserify files
 module.exports = function(b) {
-  var cloneSink = clone.sink();
   return b.bundle()
     .on('error', error)
     .pipe(source(config.browserify.bundle))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(cloneSink)
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-    .pipe(cloneSink.tap())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.browserify.dist))
     .pipe(browserSync.stream({once: true}));

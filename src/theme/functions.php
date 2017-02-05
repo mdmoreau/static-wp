21073175428f9ef8787ea24c15247feee7fd9acc
mdmoreau <mdmoreau@gmail.com>
2017-02-05 13:18:28 -0500

    add gravity forms ajax fix

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -96,3 +96,21 @@ add_filter('wpseo_metabox_prio', function() { return 'low'; });
 
 // move gravity forms js to footer
 add_filter('gform_init_scripts_footer', '__return_true');
+
+// modify gravity forms inline ajax
+add_filter('gform_cdata_open', 'wrap_gform_cdata_open', 1);
+function wrap_gform_cdata_open($content = '') {
+  if ((defined('DOING_AJAX') && DOING_AJAX) || isset($_POST['gform_ajax'])) {
+    return $content;
+  }
+  $content = 'document.addEventListener("DOMContentLoaded", function() {';
+  return $content;
+}
+add_filter('gform_cdata_close', 'wrap_gform_cdata_close', 99);
+function wrap_gform_cdata_close($content = '') {
+  if ((defined('DOING_AJAX') && DOING_AJAX) || isset($_POST['gform_ajax'])) {
+    return $content;
+  }
+  $content = '}, false);';
+  return $content;
+}
